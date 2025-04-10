b44e9641bc0235279cb799480c5829bb47222239
Desilea <desilea@excelia-group.com>
2025-04-10 10:03:49 +0200

    Add files via upload

---
+++ Outgrow.js
@@ -0,0 +1,21 @@
+function ogAnimationInit(e) {
+	var chatCircle = document.getElementById("bot-circle");
+	chatCircle.classList.toggle("active");
+	var chatBox = document.getElementById("og-chat-box");
+	chatBox.classList.toggle("no-animation");
+}
+
+function ogAnimationClose(e) {
+	setTimeout(() => {
+		var chatCircle = document.getElementById("bot-circle");
+		chatCircle.classList.toggle("active");
+	}, 100);
+	var chatBox = document.getElementById("og-chat-box");
+	chatBox.classList.toggle("no-animation");
+}
+
+function closeFloatBar(event) {
+	document.getElementById("bot-circle").style.display = "none";
+	event.stopPropagation();
+	event.preventDefault();
+}
